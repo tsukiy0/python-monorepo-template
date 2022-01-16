39010bc94fbc8f82a09e97941a35407e98a7868e
tsukiy0 <tsukiy0@users.noreply.github.com>
2022-01-16 04:20:22 +0000

    feat: wire up save handler

--- py/api/api/app.py
+++ py/api/api/app.py
@@ -1,4 +1,11 @@
 from fastapi import FastAPI
+from libs.core.handlers.save_address_handler import (
+    SaveAddressHandler,
+    SaveAddressRequest,
+)
+from libs.infrastructure.services.memory_address_repository import (
+    MemoryAddressRepository,
+)
 
 app = FastAPI()
 
@@ -6,3 +13,9 @@ app = FastAPI()
 @app.get("/hello")
 async def hello():
     return "hello!"
+
+
+@app.get("/address/save")
+async def save(request: SaveAddressRequest):
+    handler = SaveAddressHandler(address_repository=MemoryAddressRepository())
+    await handler.handle(request)
